317f04841cc3ac9c541a081e17d2226d2840b7a6
conmeconte <32534202+conmeconte@users.noreply.github.com>
2018-01-29 01:03:11 -0800

    finished feature 5

---
+++ src/components/contact_form.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import Field from './field';
+
+class ContactForm extends Component{
+    constructor(props){
+        super(props);
+
+        this.handleInputChange= this.handleInputChange.bind(this); 
+        this.handleSubmit= this.handleSubmit.bind(this);
+
+        this.state={
+            form:{
+                firstName: '',
+                lastName: '',
+                phone: '',
+                email: ''
+            }
+        }
+    }
+    handleInputChange(event){
+
+        const {value, name} = event.target; 
+        const {form}= this.state; 
+        form[name]=value; 
+        this.setState({
+            form: {...form}
+        })
+
+    }
+
+    handleSubmit(){
+        event.preventDefault();
+        event.stopPropagation();
+        console.log(this.state.form);
+    }
+    render(){
+        const {firstName, lastName, phone, email}= this.state.form; 
+
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <Field name="firstName" label="First Name" type="text" value={firstName} onChange={this.handleInputChange}/>
+                <Field name="lastName" label="Last Name" type="text" value={lastName} onChange={this.handleInputChange}/>
+                <Field name="phone" label="Phone Number" type="tel" value={phone} onChange={this.handleInputChange}/>
+                <Field name="email" label="Email" type="email" value={email} onChange={this.handleInputChange}/>
+                <button>Add Contact</button>
+            </form>
+        )
+    }
+}
+
+export default ContactForm; 
