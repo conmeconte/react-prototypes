1c73e5b310a2fcec00ab7859814d36e8ca753144
conmeconte <32534202+conmeconte@users.noreply.github.com>
2018-01-29 21:57:23 -0800

    finished required features

---
+++ src/components/image_data.js
@@ -0,0 +1,72 @@
+import puppy1 from './images/papillion.jpg';
+import puppy2 from './images/2-face.jpg';
+import puppy3 from './images/3-yellowpup.jpg';
+import puppy4 from './images/images.jpg';
+import puppy5 from './images/papillon-02.jpg';
+import puppy6 from './images/run.jpg';
+import puppy7 from './images/tinkerbell.jpg';
+import puppy8 from './images/papillon-dog.jpg';
+
+export default [
+    {
+        src: puppy1,
+        style: {
+            left: '5%',
+            transform: 'rotate(-10deg)'
+        }
+    },
+    {
+        src: puppy2,
+        style: {
+            left: '40%',
+            transform: 'rotate(6deg)'
+        }
+    },
+    {
+        src: puppy3,
+        style: {
+            right: '15%',
+            transform: 'rotate(-9deg)'
+        }
+    },
+    {
+        src: puppy4,
+        style: {
+            left: '8%',
+            top: '28%',
+            transform: 'rotate(8deg)'
+        }
+    },
+    {
+        src: puppy5,
+        style: {
+            left: '37%',
+            top: '25%',
+            transform: 'rotate(-12deg)'
+        }
+    },
+    {
+        src: puppy6,
+        style: {
+            right: '12%',
+            top: '26%',
+            transform: 'rotate(10deg)'
+        }
+    },
+    {
+        src: puppy7,
+        style: {
+            left: '25%',
+            top: '48%',
+            transform: 'rotate(-5deg)'
+        }
+    },
+    {
+        src: puppy8,
+        style: {
+            right: '25%',
+            top: '52%',
+            transform: 'rotate(16deg)'
+        }
+    }
+];
---
+++ src/components/scrapbook_images.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default (props)=>{
+    console.log(props);
+    const {src, style}= props.about; 
+    return(
+        <img className='scrapbook-image' src={src} style={style}/>
+    )
+}
