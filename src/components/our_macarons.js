67f636edb95d16ac3f23eae82938b48fa759c7ec
conmeconte <32534202+conmeconte@users.noreply.github.com>
2018-02-03 00:15:31 -0800

    finished routing-intro"

---
+++ src/components/our_macarons.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props=>{
+    return(
+        <div>
+            <h1>Our Macarons</h1>
+            <p>They are very good</p>      
+        </div>
+    );
+}
---
+++ src/components/welcome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props=>{
+    return(
+        <div>
+            <h1>Welcome</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quidem minima itaque nihil suscipit placeat debitis ipsum sed atque totam aliquid fuga, magnam earum? Alias, doloremque. A dolor necessitatibus soluta.</p>
+        </div>
+    );
+}
